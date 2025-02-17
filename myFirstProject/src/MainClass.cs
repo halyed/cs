9712617b3ddefcd0f8b782bae5938a7346900484
Hamed LY <mangali226@yahoo.fr>
2025-02-17 23:04:56 +0100

    cs day 2

--- myFirstProject/src/MainClass.cs
+++ myFirstProject/src/MainClass.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Threading.Tasks;
 
 namespace myFirstProject.src
 {
     public class MainClass
     {
+        const String country = "FRANCE";
         public static void Main(){
             Console.WriteLine("Hello world");
             //Console.Write("What is your name : ");
@@ -20,6 +22,8 @@ namespace myFirstProject.src
             int result = setVars(4, 5);
             Console.WriteLine(result);
             printVarsTypes();
+            usingOperators(0, 0);
+            printDate();
             
         }
 
@@ -35,8 +39,36 @@ namespace myFirstProject.src
             double rating = 99.50;
             float ratingFloat = 99.50f;
             byte numberOfEmployees = 255; // limited to 255
+            const double pi = 3.14;
+            String s2 = String.Empty;
 
-            Console.WriteLine(name, monthlyWage, isActive, rating, ratingFloat, numberOfEmployees);
+            Console.WriteLine(name + ", " + monthlyWage + ", " + isActive + ", " + rating + ", " + ratingFloat + ", " + numberOfEmployees + ", " + pi + ", " + s2);
+
+        }
+
+        public static void usingOperators(BigInteger a, BigInteger b){
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(float.MaxValue);
+            char userSelection = 'a';
+            char upperVersion = char.ToUpper(userSelection);
+            Console.WriteLine(userSelection);
+            bool isLetter = char.IsLetter(userSelection);
+            Console.WriteLine(isLetter);
+
+        }
+
+        public static void printDate(){
+            DateTime employeeStartDate = new DateTime(2025, 02, 17);
+            DateTime today = DateTime.Today;
+            DateTime now = DateTime.Now;
+            TimeSpan workTime = new TimeSpan(8, 0, 0);
+            DateTime twoDaysLater = today.AddDays(2);
+            DayOfWeek dayOfWeek = twoDaysLater.DayOfWeek;
+            bool isDST = twoDaysLater.IsDaylightSavingTime();
+
+            Console.WriteLine(employeeStartDate + ", " + today + ", " + now + ", " + ", " + workTime + ", " + twoDaysLater + ", " + dayOfWeek + ", " + isDST );
+            
 
         }
     }

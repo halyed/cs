16316c0fe02d3f0ae85588f81bd2deaa35586155
Hamed LY <mangali226@yahoo.fr>
2025-02-25 23:05:05 +0100

    cs day 4

--- myFirstProject/src/MainClass.cs
+++ myFirstProject/src/MainClass.cs
@@ -19,85 +19,31 @@ namespace myFirstProject.src
             Console.WriteLine($"{Environment.NewLine} Hello, " + name + ", we are on {curDate}");
             Console.Write("Teslicapota");*/
 
-            int result = setVars(4, 5);
+            int result = Syntax.setVars(4, 5);
             Console.WriteLine(result);
-            printVarsTypes();
-            usingOperators(0, 0);
-            printDate();
+            Syntax.printVarsTypes();
+            Syntax.usingOperators(0, 0);
+            Syntax.printDate();
             String type = "switch";
-            isGreaterThanTwo(1,type);
+            Syntax.isGreaterThanTwo(1,type);
+            int facto = Syntax.usingIterationForFactoriel(5, "for");
+            Console.WriteLine("result of factoriel : " + facto);
+            // calling the optional method
+            int sumOf = Syntax.sumWithOptional(3,5);
+            Console.WriteLine("result of first sumOf : " + sumOf);
+            int sumOf2 = Syntax.sumWithOptional(4,5,11);
+            Console.WriteLine("result of second sumOf : " +sumOf2);
+            // using named parameters
+            int sumOf3 = Syntax.sumWithOptional(a:4,b:5,c:21);
+            Console.WriteLine("result of third sumOf : " +sumOf3);
+            // using expression bodied method
+            int result2 = Syntax.expressionMethodSumWithOptional(20, 20, 20);
+            Console.WriteLine($"result of sum with expression bodied method : {result2}");
+
             
         }
 
-        public static int setVars(int a, int b){
-            return a + b;
-        }
-
-
-        public static void printVarsTypes(){
-            string name = "Hamouda";
-            int monthlyWage = 34776;
-            bool isActive = false;
-            double rating = 99.50;
-            float ratingFloat = 99.50f;
-            byte numberOfEmployees = 255; // limited to 255
-            const double pi = 3.14;
-            String s2 = String.Empty;
-
-            Console.WriteLine(name + ", " + monthlyWage + ", " + isActive + ", " + rating + ", " + ratingFloat + ", " + numberOfEmployees + ", " + pi + ", " + s2);
-
-        }
-
-        public static void usingOperators(BigInteger a, BigInteger b){
-            Console.WriteLine(int.MaxValue);
-            Console.WriteLine(int.MinValue);
-            Console.WriteLine(float.MaxValue);
-            char userSelection = 'a';
-            char upperVersion = char.ToUpper(userSelection);
-            Console.WriteLine(userSelection);
-            bool isLetter = char.IsLetter(userSelection);
-            Console.WriteLine(isLetter);
-
-        }
-
-        public static void printDate(){
-            DateTime employeeStartDate = new DateTime(2025, 02, 17);
-            DateTime today = DateTime.Today;
-            DateTime now = DateTime.Now;
-            TimeSpan workTime = new TimeSpan(8, 0, 0);
-            DateTime twoDaysLater = today.AddDays(2);
-            DayOfWeek dayOfWeek = twoDaysLater.DayOfWeek;
-            bool isDST = twoDaysLater.IsDaylightSavingTime();
-            var monthlyWageOfHamouda = 3500;
-
-            Console.WriteLine(employeeStartDate + ", " + today + ", " + now + ", " + ", " + workTime + ", " + twoDaysLater + ", " + dayOfWeek + ", " + isDST );
-            
-
-        }
-
-        public static bool isGreaterThanTwo(int number, String operatorUsed){
-            Console.WriteLine(operatorUsed);
-            if(operatorUsed.Equals("bool")){
-                Console.WriteLine("type used boolean");
-                return number >=2;
-            }else if(operatorUsed.Equals("switch")){
-                Console.WriteLine("type used swicth");
-                switch(number){
-                    case < 2 : 
-                        Console.WriteLine("number lower than two");
-                        return false;
-                    default:
-                        Console.WriteLine("numbre greater than two");
-                        return true;
-                }
-            }else{
-                Console.WriteLine("type used if/else");
-                if (number >= 2) return true;
-                else return false;
-            }
-            
-            
-        }
+        
 
 
     }
--- myFirstProject/src/Syntax.cs
+++ myFirstProject/src/Syntax.cs
@@ -1,12 +1,126 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using System.Reflection.Metadata;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace myFirstProject.src
 {
     public abstract class Syntax
     {
+        public static int setVars(int a, int b){
+            return a + b;
+        }
+
+
+        public static void printVarsTypes(){
+            string name = "Hamouda";
+            int monthlyWage = 34776;
+            bool isActive = false;
+            double rating = 99.50;
+            float ratingFloat = 99.50f;
+            byte numberOfEmployees = 255; // limited to 255
+            const double pi = 3.14;
+            String s2 = String.Empty;
+
+            Console.WriteLine(name + ", " + monthlyWage + ", " + isActive + ", " + rating + ", " + ratingFloat + ", " + numberOfEmployees + ", " + pi + ", " + s2);
+
+        }
+
+        public static void usingOperators(System.Numerics.BigInteger a, BigInteger b){
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(float.MaxValue);
+            char userSelection = 'a';
+            char upperVersion = char.ToUpper(userSelection);
+            Console.WriteLine(userSelection);
+            bool isLetter = char.IsLetter(userSelection);
+            Console.WriteLine(isLetter);
+
+        }
+
+        public static void printDate(){
+            DateTime employeeStartDate = new DateTime(2025, 02, 17);
+            DateTime today = DateTime.Today;
+            DateTime now = DateTime.Now;
+            TimeSpan workTime = new TimeSpan(8, 0, 0);
+            DateTime twoDaysLater = today.AddDays(2);
+            DayOfWeek dayOfWeek = twoDaysLater.DayOfWeek;
+            bool isDST = twoDaysLater.IsDaylightSavingTime();
+            var monthlyWageOfHamouda = 3500;
+
+            Console.WriteLine(employeeStartDate + ", " + today + ", " + now + ", " + ", " + workTime + ", " + twoDaysLater + ", " + dayOfWeek + ", " + isDST );
+            
+
+        }
+
+        public static bool isGreaterThanTwo(int number, String operatorUsed){
+            Console.WriteLine(operatorUsed);
+            if(operatorUsed.Equals("bool")){
+                Console.WriteLine("type used boolean");
+                return number >=2;
+            }else if(operatorUsed.Equals("switch")){
+                Console.WriteLine("type used swicth");
+                switch(number){
+                    case < 2 : 
+                        Console.WriteLine("number lower than two");
+                        return false;
+                    default:
+                        Console.WriteLine("numbre greater than two");
+                        return true;
+                }
+            }else{
+                Console.WriteLine("type used if/else");
+                if (number >= 2) return true;
+                else return false;
+            }
+        }
+
+        public static int sumWithOptional(int a, int b, int c = 100){
+            return a + b + c;
+        }
+
+        public static int expressionMethodSumWithOptional(int a, int b, int c) => a + b + c;
+
+        public static int usingIterationForFactoriel(int number, string iterator){
+            int newNumber = 1;
+            if(iterator.Equals("while")){
+                Console.WriteLine("Using while loop");
+                if(number == 0 || number == 1) return 1;
+                else{
+                    while(number > 1){
+                        newNumber  = (number * newNumber);
+                        number --;
+                    }
+                }
+            }
+            if(iterator.Equals("for")){
+                Console.WriteLine("Using for loop");
+                if(number == 0 || number == 1) newNumber = 1;
+                else{
+                    Console.WriteLine(newNumber);
+                    
+                    for(int i=1; i<=number; i++){
+                        newNumber = newNumber * i;
+                        //newNumber = 10 * 10;
+                    }
+                    Console.WriteLine(newNumber);
+                    Console.WriteLine(number);
+                }
+            }
+            if(iterator.Equals("do while")){
+                Console.WriteLine("Using do while loop");
+                if(number == 0 || number == 1) return 1;
+                else{
+                    for(int i=1; i<=number; i++){
+                        newNumber = newNumber  * i;
+                    }
+                }
+            }
+            return newNumber;
+        }
         
     }
 }
